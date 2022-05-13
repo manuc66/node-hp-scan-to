f4f203832900e167c33846683cbfeeef08733058
manuc66 <manuc66@gmail.com>
2022-05-14 01:22:26 +0200

    add tests

--- test/EtagEventTable.test.ts
+++ test/EtagEventTable.test.ts
@@ -45,4 +45,43 @@ describe("EtagEventTable", () => {
       expect(etagEventTable.eventTable.events[0].agingStamp).to.be.eq("139-1");
     });
   });
+  describe("Parsing eventTableComp.xml", async () => {
+    let etagEventTable: EtagEventTable;
+
+    before(async () => {
+      const content: string = await fs.readFile(
+        path.resolve(
+          __dirname,
+          "./asset/eventTableComp.xml"
+        ),
+        { encoding: "utf8" }
+      );
+      etagEventTable = await HPApi.createEtagEventTable( content, "tag1");
+    });
+
+    it("tag", async () => {
+      expect(etagEventTable.etag).to.be.eq("tag1");
+    });
+
+    it("Parse event compEventURI", async () => {
+      expect(etagEventTable.eventTable.events[0].compEventURI).to.be.eq(undefined);
+    });
+
+    it("Parse event destinationURI", async () => {
+      expect(etagEventTable.eventTable.events[0].destinationURI).to.be.eq(undefined);
+      expect(etagEventTable.eventTable.events[3].destinationURI).to.be.eq("/WalkupScanToComp/WalkupScanToCompDestinations/ab60a563-5a97-4386-bd36-ec63cbbff933");
+    });
+
+    it("Parse event isScanEvent", async () => {
+      expect(etagEventTable.eventTable.events[0].isScanEvent).to.be.eq(false);
+      expect(etagEventTable.eventTable.events[3].isScanEvent).to.be.eq(true);
+    });
+    it("Parse event unqualifiedEventCategory", async () => {
+      expect(etagEventTable.eventTable.events[0].unqualifiedEventCategory).to.be.eq("DeviceCapabilitiesChanged");
+      expect(etagEventTable.eventTable.events[3].unqualifiedEventCategory).to.be.eq("ScanEvent");
+    });
+    it("Parse event agingStamp", async () => {
+      expect(etagEventTable.eventTable.events[0].agingStamp).to.be.eq("1-16");
+    });
+  });
 });
