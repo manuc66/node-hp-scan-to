2870f0de383bbcce55cf0e0e2640fca8773bcfc2
manuc66 <manuc66@gmail.com>
2022-05-14 01:03:22 +0200

    fix tests

--- test/PathHelper.test.ts
+++ test/PathHelper.test.ts
@@ -18,7 +18,7 @@ describe("PathHelper", () => {
           "" +
           (now.getMonth() + 1)
         ).padStart(2, "0")}.${("" + now.getFullYear()).padStart(4, "0")}_${(
-          "" + (now.getHours() > 12 ? now.getHours() - 12 : now.getHours())
+          "" + (now.getHours())
         ).padStart(2, "0")}:${("" + now.getMinutes()).padStart(2, "0")}:${(
           "" + now.getSeconds()
         ).padStart(2, "0")}.jpg`
