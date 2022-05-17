c6b4ddbea19d91b2b7798344859b82be63f952a9
manuc66 <manuc66@gmail.com>
2022-05-17 22:27:52 +0200

    implement PathHelper.makeUnique

--- src/PathHelper.ts
+++ src/PathHelper.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import dateformat from "dateformat";
-import fs from "fs/promises";
+const { promises: Fs } = require("fs");
+const fs = require("fs");
 import os from "os";
 
 export default class PathHelper {
@@ -8,36 +9,69 @@ export default class PathHelper {
     folder: string,
     scanCount: number,
     currentPageNumber: number,
-    filePattern : string | undefined,
+    filePattern: string | undefined,
     extension: string
   ): string {
-
     if (filePattern) {
-      return path.join(folder, `${dateformat(new Date(), filePattern)}.${extension}`);
+      return path.join(
+        folder,
+        `${dateformat(new Date(), filePattern)}.${extension}`
+      );
     }
 
-    return path.join(folder, `scan${scanCount}_page${currentPageNumber}.${extension}`);
+    return path.join(
+      folder,
+      `scan${scanCount}_page${currentPageNumber}.${extension}`
+    );
+  }
+
+  static makeUnique(filePath: string): string {
+    if (!fs.existsSync(filePath)) {
+      return filePath;
+    }
+
+    let parsed = path.parse(filePath);
+    let tryName = `${parsed.dir}${path.sep}${parsed.name}${dateformat(
+      new Date(),
+      "yyyymmdd"
+    )}${parsed.ext}`;
+    if (!fs.existsSync(tryName)) {
+      return tryName;
+    }
+
+    parsed = path.parse(tryName);
+    let i = "a";
+    while (i <= "z") {
+      tryName = `${parsed.dir}${path.sep}${parsed.name}_${i}${parsed.ext}`;
+      if (!fs.existsSync(tryName)) {
+        return tryName;
+      }
+      i = String.fromCharCode(i.charCodeAt(0) + 1);
+    }
+    throw new Error(
+      `Can not create unique file: ${filePath} iterated until: ${tryName}`
+    );
   }
 
   static getFileForScan(
     folder: string,
     scanCount: number,
-    filePattern : string | undefined,
+    filePattern: string | undefined,
     extension: string
   ): string {
-
     if (filePattern) {
-      return path.join(folder, `${dateformat(new Date(), filePattern)}.${extension}`);
+      return path.join(
+        folder,
+        `${dateformat(new Date(), filePattern)}.${extension}`
+      );
     }
 
     return path.join(folder, `scan${scanCount}.${extension}`);
   }
 
-  static async getOutputFolder(folder: string | undefined) {
-    if (!folder) {
-      folder = await fs.mkdtemp(
-        path.join(os.tmpdir(), "scan-to-pc")
-      );
+  static async getOutputFolder(folder?: string | undefined): Promise<string> {
+    if (typeof folder !== "string") {
+      return Fs.mkdtemp(path.join(os.tmpdir(), "scan-to-pc"));
     }
     return folder;
   }
