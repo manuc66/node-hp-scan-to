06c47d432200623e3410469f2fb3039154998bc4
manuc66 <manuc66@gmail.com>
2023-03-16 00:01:06 +0100

    Allow to configure device up polling interval Allow to configure autoscan_duplex, autoscan_pdf, autoscan_pollingInterval and autoscan_startScanDelay

--- src/HPApi.ts
+++ src/HPApi.ts
@@ -106,7 +106,7 @@ export default class HPApi {
     });
   }
 
-  static async waitDeviceUp(): Promise<void> {
+  static async waitDeviceUp(deviceUpPollingInterval: number): Promise<void> {
     let first = true;
     while (!(await HPApi.isAlive())) {
       if (first) {
@@ -115,7 +115,7 @@ export default class HPApi {
         );
       }
       first = false;
-      await delay(1000);
+      await delay(deviceUpPollingInterval);
     }
     if (!first) {
       console.log(
