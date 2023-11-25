f18433ed2b63d9cb3fa1a2c9ced25c2fc1bd1391
manuc66 <manuc66@gmail.com>
2023-11-25 22:14:09 +0100

    Allow to configure the width and height from the command line

--- src/scanProcessing.ts
+++ src/scanProcessing.ts
@@ -350,6 +350,48 @@ function isPdf(
   }
 }
 
+export function getScanWidth(
+  scanConfig: ScanConfig,
+  inputSource: "Adf" | "Platen",
+  deviceCapabilities: DeviceCapabilities,
+) {
+  if (scanConfig.width && scanConfig.width > 0) {
+    const maxWidth =
+      inputSource === "Adf"
+        ? deviceCapabilities.adfMaxWidth
+        : deviceCapabilities.platenMaxWidth;
+
+    if (maxWidth && scanConfig.width > maxWidth) {
+      return maxWidth;
+    } else {
+      return scanConfig.width;
+    }
+  } else {
+    return null;
+  }
+}
+
+export function getScanHeight(
+  scanConfig: ScanConfig,
+  inputSource: "Adf" | "Platen",
+  deviceCapabilities: DeviceCapabilities,
+) {
+  if (scanConfig.height && scanConfig.height > 0) {
+    const maxHeight =
+      inputSource === "Adf"
+        ? deviceCapabilities.adfMaxHeight
+        : deviceCapabilities.platenMaxHeight;
+
+    if (maxHeight && scanConfig.height > maxHeight) {
+      return maxHeight;
+    } else {
+      return scanConfig.height;
+    }
+  } else {
+    return null;
+  }
+}
+
 export async function saveScan(
   event: Event,
   folder: string,
@@ -396,17 +438,15 @@ export async function saveScan(
   console.log("Afd is : " + scanStatus.adfState);
 
   const inputSource = scanStatus.getInputSource();
+  const scanWidth = getScanWidth(scanConfig, inputSource, deviceCapabilities);
+  const scanHeight = getScanHeight(scanConfig, inputSource, deviceCapabilities);
 
   const scanJobSettings = new ScanJobSettings(
     inputSource,
     contentType,
     scanConfig.resolution,
-    inputSource === "Adf"
-      ? deviceCapabilities.adfMaxWidth
-      : deviceCapabilities.platenMaxWidth,
-    inputSource === "Adf"
-      ? deviceCapabilities.adfMaxHeight
-      : deviceCapabilities.platenMaxHeight,
+    scanWidth,
+    scanHeight,
     isDuplex,
   );
 
@@ -447,6 +487,8 @@ export type DirectoryConfig = {
 };
 export type ScanConfig = {
   resolution: number;
+  width: number | null;
+  height: number | null;
   directoryConfig: DirectoryConfig;
 };
 export type AdfAutoScanConfig = ScanConfig & {
@@ -461,6 +503,7 @@ export async function scanFromAdf(
   folder: string,
   tempFolder: string,
   adfAutoScanConfig: AdfAutoScanConfig,
+  deviceCapabilities: DeviceCapabilities,
 ) {
   let destinationFolder: string;
   let contentType: "Document" | "Photo";
@@ -475,12 +518,19 @@ export async function scanFromAdf(
     destinationFolder = folder;
   }
 
+  const scanWidth = getScanWidth(adfAutoScanConfig, "Adf", deviceCapabilities);
+  const scanHeight = getScanHeight(
+    adfAutoScanConfig,
+    "Adf",
+    deviceCapabilities,
+  );
+
   const scanJobSettings = new ScanJobSettings(
     "Adf",
     contentType,
     adfAutoScanConfig.resolution,
-    null,
-    null,
+    scanWidth,
+    scanHeight,
     adfAutoScanConfig.isDuplex,
   );
 
---
+++ test/scanProcessing.test.ts
@@ -0,0 +1,108 @@
+import { describe } from "mocha";
+import { expect } from "chai";
+import {
+  getScanWidth,
+  getScanHeight,
+  ScanConfig,
+} from "../src/scanProcessing";
+import { DeviceCapabilities } from "../src/DeviceCapabilities";
+
+describe("scanProcessing", () => {
+  let scanConfig: ScanConfig;
+  let deviceCapabilities: DeviceCapabilities;
+
+  beforeEach(async () => {
+    scanConfig = {
+      resolution: 200,
+      width: null,
+      height: null,
+      directoryConfig: {
+        directory: undefined,
+        tempDirectory: undefined,
+        filePattern: undefined,
+      },
+    };
+    deviceCapabilities = {
+      supportsMultiItemScanFromPlaten: false,
+      useWalkupScanToComp: false,
+      platenMaxWidth: null,
+      platenMaxHeight: null,
+      adfMaxWidth: null,
+      adfMaxHeight: null,
+    };
+  });
+
+  describe("getScanWidth", async () => {
+    const inputSource = "Adf";
+    describe("Adf", async () => {
+      it("Does not define a value if nothing provided", async () => {
+        const width = getScanWidth(scanConfig, inputSource, deviceCapabilities);
+        expect(width).to.be.eq(null);
+      });
+      it("Does not define a value if negative provided", async () => {
+        scanConfig.width = -1;
+        const width = getScanWidth(scanConfig, inputSource, deviceCapabilities);
+        expect(width).to.be.eq(null);
+      });
+      it("Define the value if no max available from device", async () => {
+        scanConfig.width = 2583;
+        const width = getScanWidth(scanConfig, inputSource, deviceCapabilities);
+        expect(width).to.be.eq(2583);
+      });
+    });
+    describe("Platen", async () => {
+      const inputSource = "Platen";
+      it("Does not define a value if nothing provided", async () => {
+        const width = getScanWidth(scanConfig, inputSource, deviceCapabilities);
+        expect(width).to.be.eq(null);
+      });
+      it("Does not define a value if negative provided", async () => {
+        scanConfig.width = -1;
+        const width = getScanWidth(scanConfig, inputSource, deviceCapabilities);
+        expect(width).to.be.eq(null);
+      });
+      it("Define the value if no max available from device", async () => {
+        scanConfig.width = 2583;
+        const width = getScanWidth(scanConfig, inputSource, deviceCapabilities);
+        expect(width).to.be.eq(2583);
+      });
+    });
+  });
+
+  describe("getScanHeight", async () => {
+    const inputSource = "Adf";
+    describe("Adf", async () => {
+      it("Does not define a value if nothing provided", async () => {
+        const height = getScanHeight(scanConfig, inputSource, deviceCapabilities);
+        expect(height).to.be.eq(null);
+      });
+      it("Does not define a value if negative provided", async () => {
+        scanConfig.height = -1;
+        const height = getScanHeight(scanConfig, inputSource, deviceCapabilities);
+        expect(height).to.be.eq(null);
+      });
+      it("Define the value if no max available from device", async () => {
+        scanConfig.height = 1269;
+        const width = getScanHeight(scanConfig, inputSource, deviceCapabilities);
+        expect(width).to.be.eq(1269);
+      });
+    });
+    describe("Platen", async () => {
+      const inputSource = "Platen";
+      it("Does not define a value if nothing provided", async () => {
+        const height = getScanHeight(scanConfig, inputSource, deviceCapabilities);
+        expect(height).to.be.eq(null);
+      });
+      it("Does not define a value if negative provided", async () => {
+        scanConfig.height = -1;
+        const height = getScanHeight(scanConfig, inputSource, deviceCapabilities);
+        expect(height).to.be.eq(null);
+      });
+      it("Define the value if no max available from device", async () => {
+        scanConfig.height = 1269;
+        const height = getScanHeight(scanConfig, inputSource, deviceCapabilities);
+        expect(height).to.be.eq(1269);
+      });
+    });
+  });
+});
