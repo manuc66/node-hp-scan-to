496469ebbc03199020674efc93913e5409b8467a
manuc66 <manuc66@gmail.com>
2023-11-25 20:51:09 +0100

    running prettier

--- src/ScanCaps.ts
+++ src/ScanCaps.ts
@@ -5,20 +5,20 @@ import { promisify } from "util";
 const parseString = promisify<string, ScanCapsData>(parser.parseString);
 
 export interface ScanCapsData {
-  "ScanCaps": {
-    "Platen": {
-      "InputSourceCaps": {
-        "MaxWidth": number,
-        "MaxHeight": number
-      }[]
-    }[],
-    "Adf": {
-      "InputSourceCaps": {
-        "MaxWidth": number,
-        "MaxHeight": number
-      }[]
-    }[]
-  }
+  ScanCaps: {
+    Platen: {
+      InputSourceCaps: {
+        MaxWidth: number;
+        MaxHeight: number;
+      }[];
+    }[];
+    Adf: {
+      InputSourceCaps: {
+        MaxWidth: number;
+        MaxHeight: number;
+      }[];
+    }[];
+  };
 }
 
 export default class ScanCaps {
@@ -34,18 +34,28 @@ export default class ScanCaps {
   }
 
   get PlatenMaxWidth(): number | null {
-    return this.data["ScanCaps"]["Platen"][0]["InputSourceCaps"][0]["MaxWidth"] || null
+    return (
+      this.data["ScanCaps"]["Platen"][0]["InputSourceCaps"][0]["MaxWidth"] ||
+      null
+    );
   }
 
   get PlatenMaxHeight(): number | null {
-    return this.data["ScanCaps"]["Platen"][0]["InputSourceCaps"][0]["MaxHeight"] || null
+    return (
+      this.data["ScanCaps"]["Platen"][0]["InputSourceCaps"][0]["MaxHeight"] ||
+      null
+    );
   }
 
   get AdfMaxWidth(): number | null {
-    return this.data["ScanCaps"]["Adf"][0]["InputSourceCaps"][0]["MaxWidth"] || null
+    return (
+      this.data["ScanCaps"]["Adf"][0]["InputSourceCaps"][0]["MaxWidth"] || null
+    );
   }
 
   get AdfMaxHeight(): number | null {
-    return this.data["ScanCaps"]["Adf"][0]["InputSourceCaps"][0]["MaxHeight"] || null
+    return (
+      this.data["ScanCaps"]["Adf"][0]["InputSourceCaps"][0]["MaxHeight"] || null
+    );
   }
 }
