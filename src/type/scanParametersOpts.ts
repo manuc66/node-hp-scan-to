5a0d2ad0c69d9a4616ca707b43dd92347fbabbbf
manuc66 <manuc66@gmail.com>
2025-05-30 00:45:13 +0200

    Move type definition to its own file

---
+++ src/type/scanParametersOpts.ts
@@ -0,0 +1,15 @@
+//:
+export type ScanParametersOpts = {
+  resolution?: string | undefined;
+  height?: string | undefined;
+  width?: string | undefined;
+  pattern?: string | undefined;
+  tempDirectory?: string | undefined;
+  keepFiles?: boolean | undefined;
+  nextcloudUploadFolder?: string | undefined;
+  nextcloudPasswordFile?: string | undefined;
+  nextcloudPassword?: string | undefined;
+  nextcloudUsername?: string | undefined;
+  nextcloudUrl?: string | undefined;
+  directory?: string | undefined;
+};
