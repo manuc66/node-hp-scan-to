e17f1e2899b5a0fdb0c539f3745d0189b1ed39c7
manuc66 <manuc66@gmail.com>
2026-05-20 00:06:14 +0200

    Refactor adfAutoscanCmd for lower complexity and add missing test assertions

--- src/commands/adfAutoscanCmd.ts
+++ src/commands/adfAutoscanCmd.ts
@@ -1,18 +1,35 @@
 import HPApi from "../HPApi.js";
 import { readDeviceCapabilities } from "../readDeviceCapabilities.js";
 import { scanFromAdf, waitAdfLoaded } from "../scanProcessing.js";
-
-import type { AdfAutoScanConfig } from "../type/scanConfigs.js";
 import PathHelper from "../PathHelper.js";
+import type { DeviceCapabilities } from "../type/DeviceCapabilities.js";
+import type { AdfAutoScanConfig } from "../type/scanConfigs.js";
 
 let iteration = 0;
+
+function checkCapabilities(
+  adfAutoScanConfig: AdfAutoScanConfig,
+  deviceCapabilities: DeviceCapabilities,
+) {
+  if (!deviceCapabilities.hasAdfDetectPaperLoaded) {
+    console.log(
+      "WARNING: The automatic scan feature is likely unsupported on this device, as its advertised capabilities do not include this feature.",
+    );
+  }
+
+  if (adfAutoScanConfig.isDuplex && !deviceCapabilities.hasAdfDuplex) {
+    console.log(
+      "WARNING: The requested duplex scan method is likely unsupported on this device, as its advertised capabilities do not include this feature.",
+    );
+  }
+}
+
 export async function adfAutoscanCmd(
   adfAutoScanConfig: AdfAutoScanConfig,
   deviceUpPollingInterval: number,
 ): Promise<void> {
   // first make sure the device is reachable
   await HPApi.waitDeviceUp(deviceUpPollingInterval);
-  let deviceUp = true;
 
   const folder = await PathHelper.getTargetFolder(
     adfAutoScanConfig.directoryConfig.directory,
@@ -25,21 +42,13 @@ export async function adfAutoscanCmd(
     adfAutoScanConfig.preferEscl,
   );
 
-  if (!deviceCapabilities.hasAdfDetectPaperLoaded) {
-    console.log(
-      "WARNING: The automatic scan feature is likely unsupported on this device, as its advertised capabilities do not include this feature.",
-    );
-  }
-
-  if (adfAutoScanConfig.isDuplex && !deviceCapabilities.hasAdfDuplex) {
-    console.log(
-      "WARNING: The requested duplex scan method is likely unsupported on this device, as its advertised capabilities do not include this feature.",
-    );
-  }
+  checkCapabilities(adfAutoScanConfig, deviceCapabilities);
 
   let scanCount = 0;
   let keepActive = true;
   let errorCount = 0;
+  let deviceUp = true;
+
   while (keepActive) {
     iteration++;
     console.log(`Iteration ${iteration} (Errors so far: ${errorCount})`);
@@ -81,6 +90,7 @@ export async function adfAutoscanCmd(
 
     if (!deviceUp) {
       await HPApi.waitDeviceUp(deviceUpPollingInterval);
+      deviceUp = true;
     } else {
       await HPApi.delay(1000);
     }
