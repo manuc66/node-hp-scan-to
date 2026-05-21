c6247d5a4bb00fae2f921f6d4c5df4f7edb5f94f
manuc66 <manuc66@gmail.com>
2026-05-21 21:04:23 +0200

    test(adfAutoscanCmd): add tests for error handling, recovery logic, and successful scan iterations

--- test/adfAutoscanCmd.test.ts
+++ test/adfAutoscanCmd.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, beforeEach, afterEach } from "mocha";
+import { expect } from "chai";
 import nock from "nock";
 import HPApi from "../src/HPApi.js";
 import { adfAutoscanCmd } from "../src/commands/adfAutoscanCmd.js";
@@ -121,15 +122,272 @@ describe("adfAutoscanCmd", () => {
     nock("http://127.0.0.1:80").persist().get("/Scan/Status").reply(500);
 
     HPApi.isAlive = async () => true;
-    // Mock HPApi.delay to return instantly
-    HPApi.delay = async () => {
-      /* no-op */
+    HPApi.delay = async () => {};
+    HPApi.waitDeviceUp = async () => {};
+
+    await adfAutoscanCmd(config, 1);
+  });
+
+  it("should call waitDeviceUp when device goes down and recover", async () => {
+    const config: AdfAutoScanConfig = {
+      resolution: 300,
+      mode: ScanMode.Color,
+      width: undefined,
+      height: undefined,
+      format: ScanFormat.Jpeg,
+      directoryConfig: {
+        directory: tempDir,
+        tempDirectory: tempDir,
+        filePattern: undefined,
+      },
+      paperlessConfig: undefined,
+      nextcloudConfig: undefined,
+      preferEscl: false,
+      paperSize: undefined,
+      paperDim: undefined,
+      paperOrientation: undefined,
+      isDuplex: false,
+      generatePdf: false,
+      pollingInterval: 1,
+      startScanDelay: 1,
     };
-    // Mock HPApi.waitDeviceUp to return instantly
+
+    nock("http://127.0.0.1:80")
+      .get("/DevMgmt/DiscoveryTree.xml")
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<ledm:DiscoveryTree xmlns:ledm="http://www.hp.com/schemas/imaging/con/ledm/2007/09/21" xmlns:dd="http://www.hp.com/schemas/imaging/con/dictionaries/1.0/">
+  <ledm:SupportedIfc>
+    <ledm:ManifestURI>/Scan/ScanJobManifest</ledm:ManifestURI>
+    <dd:ResourceType>ledm:hpLedmScanJobManifest</dd:ResourceType>
+  </ledm:SupportedIfc>
+</ledm:DiscoveryTree>`,
+      );
+
+    nock("http://127.0.0.1:80")
+      .get("/Scan/ScanJobManifest")
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<man:Manifest xmlns:man="http://www.hp.com/schemas/imaging/con/ledm/manifest/2009/03/24" xmlns:map="http://www.hp.com/schemas/imaging/con/ledm/map/2009/03/24" xmlns:dd="http://www.hp.com/schemas/imaging/con/dictionaries/1.0/" xmlns:scan="http://www.hp.com/schemas/imaging/con/ledm/scan/2008/11/17">
+    <map:ResourceMap>
+        <map:ResourceLink>
+            <dd:ResourceURI>http://127.0.0.1</dd:ResourceURI>
+        </map:ResourceLink>
+        <map:ResourceNode>
+            <map:ResourceLink>
+                <dd:ResourceURI>/Scan/ScanCaps</dd:ResourceURI>
+            </map:ResourceLink>
+            <map:ResourceType>
+                <scan:ScanResourceType>ScanCaps</scan:ScanResourceType>
+            </map:ResourceType>
+        </map:ResourceNode>
+        <map:ResourceNode>
+            <map:ResourceLink>
+                <dd:ResourceURI>/Scan/Status</dd:ResourceURI>
+            </map:ResourceLink>
+            <map:ResourceType>
+                <scan:ScanResourceType>Status</scan:ScanResourceType>
+            </map:ResourceType>
+        </map:ResourceNode>
+    </map:ResourceMap>
+</man:Manifest>`,
+      );
+
+    nock("http://127.0.0.1:80")
+      .get("/Scan/ScanCaps")
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<ScanCaps xmlns="http://www.hp.com/schemas/imaging/con/ledm/scancaps/2008/11/17">
+    <PlatenMaxWidth>2550</PlatenMaxWidth>
+    <PlatenMaxHeight>3300</PlatenMaxHeight>
+</ScanCaps>`,
+      );
+
+    nock("http://127.0.0.1:80").persist().get("/Scan/Status").reply(500);
+
+    let waitDeviceUpCalled = false;
+    let isAliveCallCount = 0;
+    HPApi.isAlive = async () => {
+      isAliveCallCount++;
+      return isAliveCallCount > 1;
+    };
+    HPApi.delay = async () => {};
     HPApi.waitDeviceUp = async () => {
-      /* no-op */
+      waitDeviceUpCalled = true;
     };
 
     await adfAutoscanCmd(config, 1);
+
+    expect(waitDeviceUpCalled).to.be.true;
+  });
+
+  it("should execute a successful scan iteration and then terminate after 50 errors", async () => {
+    const config: AdfAutoScanConfig = {
+      resolution: 300,
+      mode: ScanMode.Color,
+      width: undefined,
+      height: undefined,
+      format: ScanFormat.Jpeg,
+      directoryConfig: {
+        directory: tempDir,
+        tempDirectory: tempDir,
+        filePattern: undefined,
+      },
+      paperlessConfig: undefined,
+      nextcloudConfig: undefined,
+      preferEscl: false,
+      paperSize: undefined,
+      paperDim: undefined,
+      paperOrientation: undefined,
+      isDuplex: false,
+      generatePdf: false,
+      pollingInterval: 1,
+      startScanDelay: 1,
+    };
+
+    // Mock DiscoveryTree
+    nock("http://127.0.0.1:80")
+      .get("/DevMgmt/DiscoveryTree.xml")
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<ledm:DiscoveryTree xmlns:ledm="http://www.hp.com/schemas/imaging/con/ledm/2007/09/21" xmlns:dd="http://www.hp.com/schemas/imaging/con/dictionaries/1.0/">
+  <ledm:SupportedIfc>
+    <ledm:ManifestURI>/Scan/ScanJobManifest</ledm:ManifestURI>
+    <dd:ResourceType>ledm:hpLedmScanJobManifest</dd:ResourceType>
+  </ledm:SupportedIfc>
+</ledm:DiscoveryTree>`,
+      );
+
+    // Mock ScanJobManifest
+    nock("http://127.0.0.1:80")
+      .get("/Scan/ScanJobManifest")
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<man:Manifest xmlns:man="http://www.hp.com/schemas/imaging/con/ledm/manifest/2009/03/24" xmlns:map="http://www.hp.com/schemas/imaging/con/ledm/map/2009/03/24" xmlns:dd="http://www.hp.com/schemas/imaging/con/dictionaries/1.0/" xmlns:scan="http://www.hp.com/schemas/imaging/con/ledm/scan/2008/11/17">
+    <map:ResourceMap>
+        <map:ResourceLink>
+            <dd:ResourceURI>http://127.0.0.1</dd:ResourceURI>
+        </map:ResourceLink>
+        <map:ResourceNode>
+            <map:ResourceLink>
+                <dd:ResourceURI>/Scan/ScanCaps</dd:ResourceURI>
+            </map:ResourceLink>
+            <map:ResourceType>
+                <scan:ScanResourceType>ScanCaps</scan:ScanResourceType>
+            </map:ResourceType>
+        </map:ResourceNode>
+        <map:ResourceNode>
+            <map:ResourceLink>
+                <dd:ResourceURI>/Scan/Status</dd:ResourceURI>
+            </map:ResourceLink>
+            <map:ResourceType>
+                <scan:ScanResourceType>Status</scan:ScanResourceType>
+            </map:ResourceType>
+        </map:ResourceNode>
+    </map:ResourceMap>
+</man:Manifest>`,
+      );
+
+    // Mock ScanCaps
+    nock("http://127.0.0.1:80")
+      .get("/Scan/ScanCaps")
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<ScanCaps xmlns="http://www.hp.com/schemas/imaging/con/ledm/scancaps/2008/11/17">
+    <AdfMaxWidth>2550</AdfMaxWidth>
+    <AdfMaxHeight>3300</AdfMaxHeight>
+    <AdfDetectPaperLoaded>true</AdfDetectPaperLoaded>
+</ScanCaps>`,
+      );
+
+    // 1st & 2nd Status request: ADF is loaded and scanner is Idle
+    nock("http://127.0.0.1:80")
+      .get("/Scan/Status")
+      .times(2)
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<ScanStatus xmlns="http://www.hp.com/schemas/imaging/con/ledm/scanstatus/2008/11/17">
+    <ScannerState>Idle</ScannerState>
+    <AdfState>Loaded</AdfState>
+</ScanStatus>`,
+      );
+
+    // ScanJob POST
+    nock("http://127.0.0.1:8080")
+      .post("/Scan/Jobs")
+      .reply(201, "", { Location: "http://127.0.0.1/Scan/Jobs/123" });
+
+    // Job Processing Status (needs to match 2x: line 242 initial check + inside waitDeviceUntilItIsReadyToUploadOrCompleted)
+    nock("http://127.0.0.1:80")
+      .get("/Scan/Jobs/123")
+      .times(2)
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<j:Job xmlns:j="http://www.hp.com/schemas/imaging/con/ledm/jobs/2009/04/30">
+    <ScanJob>
+        <PreScanPage>
+            <PageState>ReadyToUpload</PageState>
+            <BufferInfo>
+              <ScanSettings>
+                <XResolution>300</XResolution>
+                <YResolution>300</YResolution>
+                <Format>Jpeg</Format>
+                <InputSource>Adf</InputSource>
+              </ScanSettings>
+              <ImageWidth>1654</ImageWidth>
+              <ImageHeight>2338</ImageHeight>
+            </BufferInfo>
+            <BinaryURL>/Scan/Jobs/123/Pages/1</BinaryURL>
+            <PageNumber>1</PageNumber>
+        </PreScanPage>
+    </ScanJob>
+    <j:JobState>Processing</j:JobState>
+</j:Job>`,
+      );
+
+    // Job Completed Status
+    nock("http://127.0.0.1:80")
+      .get("/Scan/Jobs/123")
+      .reply(
+        200,
+        `<?xml version="1.0" encoding="UTF-8"?>
+<j:Job xmlns:j="http://www.hp.com/schemas/imaging/con/ledm/jobs/2009/04/30">
+    <ScanJob>
+        <PostScanPage>
+            <PageNumber>1</PageNumber>
+        </PostScanPage>
+    </ScanJob>
+    <j:JobState>Completed</j:JobState>
+</j:Job>`,
+      );
+
+    // Download Page
+    nock("http://127.0.0.1:8080")
+      .get("/Scan/Jobs/123/Pages/1")
+      .reply(200, "fake-image-data");
+
+    // Subsequent Status requests fail to trigger loop exit after 50 errors
+    nock("http://127.0.0.1:80")
+      .persist()
+      .get("/Scan/Status")
+      .reply(500);
+
+    HPApi.isAlive = async () => true;
+    HPApi.delay = async () => {};
+    HPApi.waitDeviceUp = async () => {};
+
+    await adfAutoscanCmd(config, 1);
+
+    // Verify a file was indeed saved during the success iteration
+    const files = fs.readdirSync(tempDir);
+    expect(files.some((f) => f.includes("scan1"))).to.be.true;
   });
 });
